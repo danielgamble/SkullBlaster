faf37f62affe7dee38d48e28809952cda3c6267c
danielgamble <dgamble94115@uat.edu>
2026-05-28 17:49:50 -0400

    Added SpriteScaler GameQuitter and SpriteMover

---
+++ Assets/Scripts/SpriteScaler.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteScaler : MonoBehaviour
+{
+
+    // Create a variable for our transform component
+    private Transform tf;
+    // Create a variable for the max we can scale in one frame draw
+    public float maxScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // Load our component into our variable
+        tf = GetComponent<Transform>();
+
+    }
+
+    // Axis 1: Scale
+    // Axis 2: Quit
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Find out how far and in which direction our axes are pressed
+        float axesValue = Input.GetAxis("Scale"); // This is between -1 and 1 -- a "percent
+        // Use that value to calculate a value between -100% and 100% of our max scale per frame
+        float scaleAmount = axesValue * maxScale;
+        // Increase the scale of our object by that amount on all axes (1, 1, 1) " scaleAmount
+        tf.localScale += Vector3.one * scaleAmount; // Vector3.one is the same as a vector with the value (1, 1, 1)
+       
+
+       
+    }
+}
