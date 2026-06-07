9949f464ad1f039f716b71aca9d47334253e8939
danielgamble <dgamble94115@uat.edu>
2026-06-07 01:55:06 -0400

    fixed shootzombie script

--- Assets/Scripts/Death/ShootZombie.cs
+++ Assets/Scripts/Death/ShootZombie.cs
@@ -9,23 +9,23 @@ public class ShootZombie : MonoBehaviour // this is a child of Death
                                             // a collider.
     {
 
-        Debug.Log("You touched the Zombie Star!!! ");
+        
 
         DeathDestroy deathDestroy = GetComponent<DeathDestroy>();
         //Health health = GetComponent<Health>(); // Attaches Health script to allow us to change the value 
                                                 // when a collision occurs.
 
-        if (deathDestroy != null) // if health is there...
+        if (deathDestroy != null) // if deathDestroy is there...
         {
             deathDestroy.Die(); // call the chomped function to destroy the player.
 
 
-            Debug.Log("You shot the Zombie Star!!!"); // tell the console that you were chomped.
+            Debug.Log("You shot the Zombie Star!!!"); // tell the console that you shot the zombie star.
 
         }
         else
         {
-            Debug.Log("Poo, I didn't get it."); // if health is not there, display a message that GetComponent failed.
+            Debug.Log("Poo, Death is taking a break."); // if death is not there, display a message that GetComponent failed.
         }
     }
 
